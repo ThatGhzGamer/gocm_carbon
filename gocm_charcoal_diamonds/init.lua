a813bf43ee6361fe48bfc16a2ebd98705fbcbf5f
ThatGhzGamer <mailbox.scott96email@gmail.com>
2019-02-16 15:05:36 -0500

    added some crafts updated modpack.conf to reflect real name
    added install instructions and more

--- gocm_charcoal_diamonds/init.lua
+++ gocm_charcoal_diamonds/init.lua
@@ -1,10 +1,19 @@
 --released under MIT license
---Diamond Crafts Go Here
 
+--Industrial Diamond
 minetest.register_craftitem("gocm_charcoal_diamonds:industrial_diamond", {
 description = "An Industrial Grade Diamond",
 inventory_image = "tgg_compressedrawdiamondv2.png",
+})
+--Recompressed Diamond
+minetest.register_craftitem("gocm_charcoal_diamonds:recompressed_diamond", {
+description = "A Recompressed Diamond",
+inventory_image = "tgg_recompresseddiamond.png",
+})
 
+
+
+--Crafts go here
 	minetest.register_craft({
 	type = "shapeless",
 	output = "default:diamond 1",
@@ -14,6 +23,18 @@ inventory_image = "tgg_compressedrawdiamondv2.png",
 	"gocm_charcoal_diamonds:industrial_diamond",
 	"gocm_charcoal_diamonds:industrial_diamond",
 	"gocm_charcoal_diamonds:industrial_diamond",
+	
+		},
+
+	minetest.register_craft({
+	type = "shapeless",
+	output = "gocm_charcoal_diamonds:recompressed_diamond 1",
+	recipe = {
+	
+	"default:diamond",
+	"default:diamond",
+	"default:diamond",
+	"default:diamond",
 	
 		},
 	})
--- readme.md
+++ readme.md
@@ -4,6 +4,14 @@ thank you.
 
 this mod is and it's textures are released under the MIT license
 
+Installation:
+Windows: download mod, unzip, copy unzipped files to your minetest-4.x or (5.x)-win32 or win64 install
+mods folder, then rename mod folder to "gocm_charcoal" without quotes 
+
+(still learning linux please comment if this is wrong)
+Ubuntu: download mod, unzip, copy unzipped files to your 
+"[yourname]/home/.minetest/mods" folder, then rename mod folder to "gocm_charcoal" without quotes 
+
 
 
 Credits Code:
